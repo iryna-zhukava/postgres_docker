0a78472742c7674526c8a9625487680a0daf7227
Iryna Zhukava <ireena.zhukova@gmail.com>
2022-03-22 18:30:25 +0300

    docker file

--- src/main/webapp/db/Connector.java
+++ src/main/webapp/db/Connector.java
@@ -3,31 +3,20 @@ package webapp.db;
 import java.sql.*;
 import java.util.Properties;
 
-public class Connector {
-    private static Connection connect() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        Properties props = new Properties();
-        props.setProperty("user", "postgres");
-        props.setProperty("password", "root");
-//        props.setProperty("ssl", "true");
-        Connection conn = DriverManager.getConnection(url, props);
+public final class Connector {
+    private Properties properties;
+    private String url;
 
-//        String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
-//        Connection conn = DriverManager.getConnection(url);
-        return conn;
+    public Connector() {
+        url = Config.DB_LINK;
+        properties = new Properties();
+        properties.setProperty("user", Config.LOGIN);
+        properties.setProperty("password", Config.PASSWORD);
+
     }
 
-    public static void main(String[] args) {
-        try {
-            Connection connection = connect();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select version()");
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1));
-            }
-        } catch (SQLException e) {
-
-
-        }
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, properties);
     }
+
 }
